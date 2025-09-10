290a91335a47dcb944456c2f9b484f9665d7ea18
sheriqa <andisheriqadewina@gmail.com>
2025-09-10 16:19:02 +0700

    <pesan_commit>

--- football_news/settings.py
+++ football_news/settings.py
@@ -32,6 +32,9 @@ DEBUG = True
 
 ...
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "a-sheriqa-footballnews.pbp.cs.ui.ac.id"]
+CSRF_TRUSTED_ORIGINS = [
+    "https://pbp.cs.ui.ac.id/a.sheriqa/footballnews"
+]
 ...
 
 
@@ -62,7 +65,7 @@ ROOT_URLCONF = 'football_news.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
